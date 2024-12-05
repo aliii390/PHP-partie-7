c597a6df7cfb8d90f7e44a745e657ae9cae808c5
aliii390 <allli.said.errahmani@gmail.com>
2024-12-05 19:01:19 +0100

    ADD: fin de l'exo6

---
+++ exo6/index.php
@@ -0,0 +1,29 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./user.php" method="POST">
+        <select name="gender" id="">
+            <option value="homme">Homme</option>
+            <option value="femme">Femme</option>
+        </select>
+
+        <label for="name">Votre nom: </label>
+        <input type="text" name="name">
+
+        <label for="firstName">Votre prenom: </label>
+        <input type="text" name="nomdefamille">
+
+
+        <input type="submit" value="Envoyer">
+    </form>
+  
+</body>
+</html>
